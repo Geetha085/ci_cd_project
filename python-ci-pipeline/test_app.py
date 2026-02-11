d2f296e8fe9cd9dbdd03b9b3f54414b38b54a59e
Geethashree <121590638+Geetha085@users.noreply.github.com>
2026-02-11 11:32:42 +0530

    Add files via upload

---
+++ python-ci-pipeline/test_app.py
@@ -0,0 +1,10 @@
+from app import add, subtract
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+
+def test_negative_numbers():
+    assert add(-1, -1) == -2
